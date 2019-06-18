0952c5ebb6b46fb6ab38f12229980074396165d5
ChalalSamir <samir-94@outlook.com>
2019-06-18 16:28:15 +0200

    aded arborescence

---
+++ assets/config/bootstrap.php
@@ -0,0 +1,9 @@
+<?php
+
+// Fichier de config principal
+
+// __DIR__ est une "constante magique"
+// elle retourne le chemin du dossier du fichier dans lequel on l'appelle
+require __DIR__ . '/param.php';
+
+require __DIR__ . '/bdd.php';
