4e4bba6aea673ea1e3ee0149a1571281a72a0028
ChalalSamir <samir-94@outlook.com>
2019-06-26 03:06:41 +0200

    article3

---
+++ article3.php
@@ -0,0 +1,45 @@
+<?php
+require 'assets/config/bootstrap.php';
+include 'assets/inc/header.php';
+$page_title = 'TchernoByLegassov - Administrateur';
+?>
+
+<section class="first">
+            <div class="first_title">
+                <h1>Un rapport <br> <span>alarmant</span></h1>
+                <div></div>
+            </div>
+        </section>
+
+    <section class="second">
+        <div class="second__image">
+        <div class="second__text">
+        <p><span>Legasov,</span>  grâce à la présence  de graphite sur le toît de la centrale, confirme l’explosion du coeur du réacteur</p>
+        <p>Le taux de radioactivité prélevé est alors très préocupant</p>
+        </div>
+        <img src="assets/images/image1.png" alt="image"/>
+        </div>
+    </section>
+
+    <section class="three">
+    <div class="three_text">
+    <p>Une question se pose ensuite pour le scientifique,</p>
+    <p class="three_text--second">Comment l’accident nucléaire - a - t-il pu se produire ?</p>
+    <div></div>
+    </div>
+    </section>
+    
+
+    <section class="foor">
+        <div class="foor_circle">
+        <p class="foor_circle_number">1</p>
+        </div>
+        <h1>Personel incompétents</h1>
+        <div class="foor__image">
+        <img src="assets/images/image 2.png" alt="image"/>
+        <div class="foor_text">
+        <p>Un test de sécurité prévu sur le réacteur n°4 de la centrale  le 25 avril, a été <span class="span_font">reporté 10h plus tard</span>. La puissance thermique du réacteur a été réduite de <span class="span_font">1 000</span> à <span class="span_font">200 MW</span>.</p>
+        <p><span class="span_white">Anatoli Diatlov</span> et son équipe ont alors commis une série d’erreurs lors du déroulé du test en violant les procédures de sérurité du réacteur.</p>
+        </div>
+        </div>
+    </section>
