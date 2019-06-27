2c2b0c94c2754a54f540dc74f0ebd63f72cc87ff
Teddy <t.boirin@icloud.com>
2019-06-27 02:35:24 +0200

    add part of back for article3

--- index.php
+++ index.php
@@ -243,7 +243,7 @@ $page_title = 'TchernoByLegassov - Administrateur';
     <section class="articleThree">
         <div class="products__product5 section--horizontal">
               <div class="first_title">
-                    <h1>Un rapport <br> <span>alarmant</span></h1>
+                    <h1 id="titleArticleThree"></h1>
                     <div></div>
                 </div>
               </div>
@@ -253,8 +253,8 @@ $page_title = 'TchernoByLegassov - Administrateur';
     <section class="second">
       <div class="second__image">
         <div class="second__text">
-          <p><span>Legasov,</span>  grâce à la présence  de graphite sur le toît de la centrale, confirme l’explosion du coeur du réacteur</p>
-          <p>Le taux de radioactivité prélevé est alors très préocupant</p>
+          <p id="contentArticleThree"></p>
+          <p id="dateArticleThree"></p>
         </div>
           <img src="assets/images/image1.png" alt="image"/>
       </div>
@@ -262,8 +262,8 @@ $page_title = 'TchernoByLegassov - Administrateur';
 
     <section class="three">
       <div class="three_text">
-        <p>Une question se pose ensuite pour le scientifique,</p>
-        <p class="three_text--second">Comment l’accident nucléaire - a - t-il pu se produire ?</p>
+        <p id="paragraphOneArticleThree"></p>
+        <p id="paragraphTwoArticleThree" class="three_text--second"></p>
         <div></div>
       </div>
     </section>
@@ -273,12 +273,12 @@ $page_title = 'TchernoByLegassov - Administrateur';
       <div class="foor_circle">
       <p class="foor_circle_number">1</p>
       </div>
-      <h1>Personel incompétents</h1>
+      <h1 id="paragraphThreeArticleThree"></h1>
       <div class="foor__image">
       <img src="assets/images/image 2.png" alt="image"/>
       <div class="foor_text">
-      <p>Un test de sécurité prévu sur le réacteur n°4 de la centrale  le 25 avril, a été <span class="span_font">reporté 10h plus tard</span>. La puissance thermique du réacteur a été réduite de <span class="span_font">1 000</span> à <span class="span_font">200 MW</span>.</p>
-      <p><span class="span_white">Anatoli Diatlov</span> et son équipe ont alors commis une série d’erreurs lors du déroulé du test en violant les procédures de sérurité du réacteur.</p>
+      <p id="paragraphFourArticleThree"></p>
+      <p id="paragraphFiveArticleThree"></p>
       </div>
       </div>
     </section>
