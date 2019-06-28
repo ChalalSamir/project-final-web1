b4798b6328f1ee2bddd190aeb4f44dbc2f965367
ChalalSamir <samir-94@outlook.com>
2019-06-28 13:55:36 +0200

    article 5

--- article5.php
+++ article5.php
@@ -5,11 +5,7 @@ include 'assets/inc/header.php';
 ?>
 
 <section class="fond">
-    <section class="articleFive_first">
-    <div class="articleFive_first_div">
-    <h1><span class="articleFive_first_margin">Mort de</span> Legasov</h1>
-    </div>
-    </section>
+
 
     <section class="articleFive_second">
     <div class="articleFive_second_div">
@@ -21,29 +17,7 @@ include 'assets/inc/header.php';
     <section class="articleFive_three">
     </section>
 
-    <section class="articleFive_for">
-    <div class="articleFive_for_div">
-    <p>Il enregistre alors un testament dans
-        lequel il dévoile toute la <span class="span_font-articleFive">vérité sur Tchernobyl</span>.</p>
-    </div>
-    </section>
 
-    <section class="articleFive_five">
-    <div class="articleFive_five_div">
-    <p><span class="span_font_size">le 22 avril 1988,<br></span> Le scientifque s’est donné la mort à son domicile,
-    <span class="span_font-articleFive">deux ans</span> après la plus grande catastrophe nucléaire de l’histoire.</p>
-    </div>
-    </section>
 
-    <section class="articleFive_six">
-    <div class="articleFive_six_div">
-    <h1>Le 20 Septembre <span class="span_font-articleFive">1996,</span></h1>
-    <p><span class="first_letter">V</span><span class="name_size">alery Legassov</span> reçoit 
-    le titre de <span class="span_font-articleFive">“Héros de la Fédération de Russie”</span>.
-     Bien que peu avare de récompenses de son vivant,
-     celui ci est enfin reconnu par l’union soviétique pour <span class="span_font-articleFive">son travail,
-    mais surtout son investissement dans l’affaire Tchernobyl</span>.</p>
-    <img src="assets/images/article5--image1.png" alt="image">
-    </div>
-    </section>
+
 </section>
--- index.php
+++ index.php
@@ -420,9 +420,45 @@ include 'assets/inc/header.php';
           
         </section>
     </div>
-  </div>
-</div> 
+
+    <div class="products__product7 section--horizontal">
+      <section class="articleFive_first">
+        <div class="articleFive_first_div">
+        <h1><span class="articleFive_first_margin">Mort de</span> Legasov</h1>
+        </div>
+      </section>
+    </div>
+
+    </div>
+  </div> 
 </section>
+
+<section class="articleFive_for">
+  <div class="articleFive_for_div">
+  <p>Il enregistre alors un testament dans
+      lequel il dévoile toute la <span class="span_font-articleFive">vérité sur Tchernobyl</span>.</p>
+  </div>
+</section>
+
+<section class="articleFive_five">
+    <div class="articleFive_five_div">
+    <p><span class="span_font_size">le 22 avril 1988,<br></span> Le scientifque s’est donné la mort à son domicile,
+    <span class="span_font-articleFive">deux ans</span> après la plus grande catastrophe nucléaire de l’histoire.</p>
+    <img src="assets/images/article5--image1.png" alt="image" class="article5-image1">
+    </div>
+</section>
+
+    <section class="articleFive_six">
+    <div class="articleFive_six_div">
+    <h1>Le 20 Septembre <span class="span_font-articleFive">1996,</span></h1>
+    <p><span class="first_letter">V</span><span class="name_size">alery Legassov</span> reçoit 
+    le titre de <span class="span_font-articleFive">“Héros de la Fédération de Russie”</span>.
+     Bien que peu avare de récompenses de son vivant,
+     celui ci est enfin reconnu par l’union soviétique pour <span class="span_font-articleFive">son travail,
+    mais surtout son investissement dans l’affaire Tchernobyl</span>.</p>
+    
+    </div>
+    </section>
 
 
 <?php include 'assets/inc/footer.php'; ?>
